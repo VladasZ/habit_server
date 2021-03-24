d6b9737b89df9b89cdaf307ee8797f3604257830
Vladas Zakrevskis <146100@gmail.com>
2021-03-24 19:15:24 +0300

    а

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ async fn main() {
     println!("start");
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hi" / String)
-        .map(|name| format!("Hello, tigidig: {}!", name));
+    let hello = warp::path!("hello")
+        .map(|| format!("Hello, tigidig!"));
 
     warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([127, 0, 0, 1], 80))
         .await;
 }
