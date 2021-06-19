5e61abc71a9c5a36449751ada54a679e0eaf40df
Vladas Zakrevksis <146100@gmail.com>
2021-06-19 15:21:53 +0300

    new login request

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,23 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
+use rocket::serde::{Serialize, Deserialize, json::Json};
 
-use serde::{ Serialize, Deserialize };
-
-use rocket_contrib::json::Json;
-use std::convert::TryFrom;
-use rocket_contrib::databases::diesel;
-
-// #[database("sqlite_logs")]
-// struct Db(diesel::SqliteConnection);
-
-#[derive(Debug, Serialize, Deserialize)]
-struct LoginData {
-    pub email: String,
-    pub password: String
+#[derive(Debug, Deserialize)]
+struct Credentials {
+    login: String,
+    password: String
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Animal {
-    pub mood: u32,
-    pub hunger: u32,
-    pub health: u32,
-
-    pub name: String,
+struct Token {
+    token: String
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    pub animal: Animal,
-    pub name: String,
-    pub age: u8,
-    pub email: String
+#[post("/login", data = "<cred>")]
+fn login(cred: Json<Credentials>) -> Json<Token> {
+    Json::from(Token { token: "kok".to_string() })
 }
 
-#[post("/login", data = "<data>")]
-fn login(mut data: Json<LoginData>) -> Json<LoginData> {
-    println!("{:?}", data);
-    data.email = "kok".to_string();
-    data.password = "sos".to_string();
-    data
-}
-
-// #[post("/login", data = "<data>")]
-// fn register(db: Db, data: Json<LoginData>) -> Json<LoginData> {
-//     //db.in
-//
-//     data
-// }
-
-#[get("/user")]
-pub fn user() -> Json<User> {
-    let mut user = User {
-        animal: Animal {
-            mood: 80,
-            hunger: 30,
-            health: 95,
-            name: "Gragdans".to_string()
-        },
-        
-        name: "Kotitka".to_string(),
-        age: 23,
-        email: "kotitka@gmail.com".to_string()
-    };
-    Json(user)
-}
-
-#[get("/hello")]
-pub fn hello() -> &'static str {
-    "Hello, outside world!"
-}
-
-fn main() {
-
-    let cfg = rocket::config::Config::build(rocket::config::Environment::Development)
-        .address("0.0.0.0")
-        .port(80)
-        .unwrap();
-
-    rocket::custom(cfg)
-    //    .attach(Db::fairing())
-        .mount("/", routes![hello, user, login])
-        .launch();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![login])
 }
