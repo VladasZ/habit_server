81760635de5db52ae6bc5d42ee75e81e9d490e4e
Vladas Zakrevksis <146100@gmail.com>
2021-09-15 20:27:50 +0300

    fixed user model

--- src/user.rs
+++ src/user.rs
@@ -1,14 +1,18 @@
 use crate::credientals::Sha3Hashable;
 use crate::Credentials;
 use rocket::serde::{Deserialize, Serialize};
+use crate::habit::Habit;
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct User {
+    #[serde(skip)]
     pub id: usize,
     pub name: String,
     pub age: u8,
     pub email: String,
     pub birthday: String,
+    pub habits: Vec<Habit>,
+    pub avatarUrl: String,
     #[serde(skip)]
     pub password_hash: String,
 }
@@ -21,6 +25,8 @@ impl User {
             age: 30,
             email: cred.email,
             birthday: "05:05:2005".to_string(),
+            habits: vec![],
+            avatarUrl: "https://crates.io/assets/Cargo-Logo-Small-c39abeb466d747f3be442698662c5260.png".into(),
             password_hash: cred.password.sha3(),
         }
     }
